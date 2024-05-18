ba603a1c8bb2a35a9d7b7902b04c540b5084e4a2
Sergej <igorami44@gmail.com>
2024-05-18 17:42:13 +0200

    Added basic heuristic

---
+++ main.py
@@ -0,0 +1,15 @@
+from gui.game import Game
+
+
+def main():
+    game = Game()
+    game.main()
+
+
+if __name__ == "__main__":
+    main()
+
+    """in board merge method blind_legal_moves with legal_moves
+    highlight doesn't work sometimes
+    change consts to enum
+    """
---
+++ robot/heuristic.py
@@ -0,0 +1,35 @@
+from gui.consts import Colors
+
+
+def get_heuristic(board):
+    matrix = board.matrix
+
+    values = {"blue": 0, "red": 0}
+
+    for x in range(8):
+        for y in range(8):
+            if (x + y) % 2 == 0:
+                checker = matrix[x][y].occupant
+                if checker is None:
+                    continue
+                color = checker.color
+                isKing = checker.king
+
+                if x != 0 and x != 7:
+                    add_by_color(values, color, 1)
+                if isKing is True:
+                    add_by_color(values, color, 10)
+                    continue
+                if color == Colors.BLUE.value:
+                    add_by_color(values, color, 7 - y)
+                else:
+                    add_by_color(values, color, y)
+
+    return values["blue"] - values["red"]
+
+
+def add_by_color(dict, color, value):
+    sel_color = "blue"
+    if color == Colors.RED.value:
+        sel_color = "red"
+    dict[sel_color] += value
