f5808884bca9f4d056eab4383f7a84f42e635a05
Sergej <igorami44@gmail.com>
2024-05-20 18:05:27 +0200

    Stable, very slow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from gui.game import Game
 
 
@@ -5,6 +7,13 @@ def main():
     game = Game()
     game.main()
 
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    filemode="w",
+    format="%(levelname)s - %(message)s",
+)
 
 if __name__ == "__main__":
     main()
--- robot/robot.py
+++ robot/robot.py
@@ -2,13 +2,25 @@ import copy
 from gui.consts import Colors
 from robot.minimax import minimax
 from robot.tree_node import TreeNode
+import logging
+import time
+
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s",
+)
 
 
 class Robot:
     def move(self, board):
-        tree = self.set_tree(TreeNode(board), 2)
-        best_node = minimax(tree, True)
+        first = time.time()
+        tree = self.set_tree(TreeNode(board), 3)
+        logging.info("Time: " + str(time.time() - first))
 
+        best_node = minimax(tree, True)
         return best_node.value
 
     def set_tree(self, tree, depth):
@@ -30,23 +42,25 @@ class Robot:
                         continue
                     ### selected a red checker
 
-                    for move in board.legal_moves((x, y)):
-                        new_board = copy.deepcopy(board)
-                        self.move_peace(new_board, (x, y), move)
-                        node = TreeNode(new_board)
-                        tree.children.append(node)
+                    self.create_children(tree, tree.value, (x, y))
 
                     for child in tree.children:
                         self.set_tree(child, depth - 1)
         return tree
 
-    def move_peace(self, board, curr_pos, mov_pos):
-        board.move_piece(curr_pos, mov_pos)
-
-        if mov_pos not in board.adjacent(curr_pos):
-            board.remove_piece(
-                (
-                    (curr_pos[0] + mov_pos[0]) >> 1,
-                    (curr_pos[1] + mov_pos[1]) >> 1,
+    def create_children(self, tree, board, curr_pos, hop=False):
+        for move in board.legal_moves(curr_pos, hop):
+            new_board = board
+            new_board.move_piece(curr_pos, move)
+            if move not in new_board.adjacent(curr_pos):
+                new_board.remove_piece(
+                    (
+                        (curr_pos[0] + move[0]) >> 1,
+                        (curr_pos[1] + move[1]) >> 1,
+                    )
                 )
-            )
+                if len(new_board.legal_moves(move, True)) != 0:  ### hop has more move
+                    self.create_children(tree, new_board, move, True)
+                    return
+            node = TreeNode(new_board)
+            tree.children.append(node)
